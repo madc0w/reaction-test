d1c459e0d71e470991dead1d6a3de16b160189d9
Marc A. Donis <marc@balloonary.io>
2023-03-13 10:32:55 +0100

    .

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 let isShowing,
 	showTime,
 	n = 0,
-	sumTimes = 0;
+	sumTimes = 0,
+	bestTime;
 
 function load() {
 	document.addEventListener('keydown', (e) => {
@@ -10,8 +11,11 @@ function load() {
 			document.getElementById('spot').classList.add('hidden');
 			const reationTime = new Date() - showTime;
 			document.getElementById('reaction-time').innerHTML = reationTime;
-
+			bestTime = Math.min(bestTime || Number.MAX_VALUE, reationTime);
+			document.getElementById('fastest-reaction-time').innerHTML = bestTime;
 			n++;
+			document.getElementById('num-trials').innerHTML = n;
+
 			sumTimes += reationTime;
 			const meanTime = sumTimes / n;
 			document.getElementById('mean-reaction-time').innerHTML =
@@ -30,4 +34,17 @@ function go() {
 		showTime = new Date();
 		isShowing = true;
 	}, 2000 + Math.random() * 2000);
+}
+
+function reset() {
+	n = 0;
+	sumTimes = 0;
+	bestTime = null;
+
+	document.getElementById('reaction-time').innerHTML = null;
+	document.getElementById('fastest-reaction-time').innerHTML = null;
+	document.getElementById('mean-reaction-time').innerHTML = null;
+	document.getElementById('spot').classList.add('hidden');
+
+	go();
 }
